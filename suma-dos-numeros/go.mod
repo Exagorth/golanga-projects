3d6a18495d872e1c414b8a217bfc41f854a94506
Exagorth <andres.claro.cordoba@gmail.com>
2025-02-16 15:30:51 -0300

    feat: añade proyecto suma-dos-numeros

---
+++ suma-dos-numeros/go.mod
@@ -0,0 +1,3 @@
+module github.com/Exagorth/golanga-projects/suma-dos-numeros
+
+go 1.23.1
---
+++ suma-dos-numeros/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+	b := 10
+
+	suma := a + b
+	fmt.Printf("La suma de %d y %d es: %d\n", a, b, suma)
+}
