326a1cf659f8bd75f0c1d2a0d742715fc186b747
Exagorth <andres.claro.cordoba@gmail.com>
2025-02-16 15:00:47 -0300

    feat: añade proyecto hello-world con estructura profesional

---
+++ hello-world/go.mod
@@ -0,0 +1,3 @@
+module github.com/Exagorth/golanga-projects/hello-world
+
+go 1.23.1
---
+++ hello-world/main.go
@@ -0,0 +1,8 @@
+package main // Indica que es un ejecutable
+
+import "fmt" // Para imprimir en consola
+
+func main() {
+	// Mensaje épico para tu primer programa
+	fmt.Println("🚀 Hello World! (Desde la estructura correcta)")
+}
